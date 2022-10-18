5398f0964c8585d968b2d1c4a608e28bdc2b9dce
unusualotter <46056832+unusualotter@users.noreply.github.com>
2022-10-18 07:27:40 -0400

    Set up modding environment for intellij

---
+++ src/main/java/net/unusualotter/randommod/RandomMod.java
@@ -0,0 +1,15 @@
+package net.unusualotter.randommod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RandomMod implements ModInitializer {
+	public static final String MOD_ID= "randommod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/unusualotter/randommod/RandomModClient.java
@@ -0,0 +1,10 @@
+package net.unusualotter.randommod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RandomModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
