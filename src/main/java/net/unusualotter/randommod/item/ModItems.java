c7fdbebf692acb7e89ca7c8f5ac02f849dbd75bf
unusualotter <46056832+unusualotter@users.noreply.github.com>
2022-10-18 14:17:45 -0400

    Blazing sword implemented correctly

--- src/main/java/net/unusualotter/randommod/item/ModItems.java
+++ src/main/java/net/unusualotter/randommod/item/ModItems.java
@@ -1,20 +1,53 @@
 package net.unusualotter.randommod.item;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
+import net.minecraft.item.*;
+import net.minecraft.recipe.Ingredient;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import net.unusualotter.randommod.RandomMod;
 
 public class ModItems {
     public static final Item BLAZING_SWORD = registerItem("blazing_sword",
-            new Item(new FabricItemSettings().group(ItemGroup.COMBAT)));
+            new SwordItem(newToolMaterial(500, 3, 2, 0, 20, Ingredient.ofItems(Items.BLAZE_ROD)), 4, 1.3f, new FabricItemSettings().group(ItemGroup.COMBAT)));
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(RandomMod.MOD_ID, name), item);
     }
 
     public static void registerModItems() {
         RandomMod.LOGGER.info("Items Registered");
+    }
+    public static ToolMaterial newToolMaterial(int durability, float miningSpeedMultiplier, float attackDamage, int miningLevel, int enchantability, Ingredient repairIngredient) {
+        return new ToolMaterial() {
+            @Override
+            public int getDurability() {
+                return durability;
+            }
+
+            @Override
+            public float getMiningSpeedMultiplier() {
+                return miningSpeedMultiplier;
+            }
+
+            @Override
+            public float getAttackDamage() {
+                return attackDamage;
+            }
+
+            @Override
+            public int getMiningLevel() {
+                return miningLevel;
+            }
+
+            @Override
+            public int getEnchantability() {
+                return enchantability;
+            }
+
+            @Override
+            public Ingredient getRepairIngredient() {
+                return repairIngredient;
+            }
+        };
     }
 }
